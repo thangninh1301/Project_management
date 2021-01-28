da6801ac2ef2287a917973690dd0f9932a31fbba
thangninh1301 <thang13199@gmail.com>
2021-01-28 14:35:36 +0700

    edit rswag

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
       mount_devise_token_auth_for 'User', at: 'auth', :skip => [:registrations],  controllers: {
           sessions: 'api/v1/sessions'
       }
-      resources :users, only: %i[index create]
       # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
     end
   end
